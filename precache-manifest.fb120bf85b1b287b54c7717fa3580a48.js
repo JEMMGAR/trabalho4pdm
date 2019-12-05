8c9083c2772047514fd8b833fa777de8c4846fe2
Edson Fagundes Garcia <jem77w@gmail.com>
2019-12-05 19:46:23 -0300

    Updates

---
+++ precache-manifest.fb120bf85b1b287b54c7717fa3580a48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc4d78a6141c7f44b715bf5c5b331ed",
+    "url": "/trabalho4pdm/index.html"
+  },
+  {
+    "revision": "ce2fb39b556a75447b62",
+    "url": "/trabalho4pdm/static/css/main.db7630d3.chunk.css"
+  },
+  {
+    "revision": "7389c1d884cab2f5c713",
+    "url": "/trabalho4pdm/static/js/2.31794a33.chunk.js"
+  },
+  {
+    "revision": "ce2fb39b556a75447b62",
+    "url": "/trabalho4pdm/static/js/main.a4bff2e4.chunk.js"
+  },
+  {
+    "revision": "d2d7a80828fcf3148983",
+    "url": "/trabalho4pdm/static/js/runtime-main.e806b6ef.js"
+  }
+]);
